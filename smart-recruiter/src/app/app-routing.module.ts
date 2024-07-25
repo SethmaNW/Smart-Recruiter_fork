2c34d10242cbef0a9df53e424588187cc8ac7f49
Ashan-Wimalasiri <e19452@eng.pdn.ac.lk>
2024-07-25 08:42:10 +0530

    routing module configuration

--- smart-recruiter/src/app/app-routing.module.ts
+++ smart-recruiter/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'nav-bar', component: NavbarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
