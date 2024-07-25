30b091c1f58cfe5c20fd93c0c83cdf3051cc171a
Ashan-Wimalasiri <e19452@eng.pdn.ac.lk>
2024-07-25 12:12:10 +0530

    admin dashboard update

--- smart-recruiter/src/app/app-routing.module.ts
+++ smart-recruiter/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   {path: 'nav-bar', component: NavbarComponent},
+  {path: 'admin-dashboard', component: AdminDashboardComponent},
 ];
 
 @NgModule({
