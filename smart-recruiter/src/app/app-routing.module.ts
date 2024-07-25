6e88ce4bb3a966ade6af618e772900ecb293762e
Ashan-Wimalasiri <e19452@eng.pdn.ac.lk>
2024-07-25 12:22:33 +0530

    coflict resove

--- smart-recruiter/src/app/app-routing.module.ts
+++ smart-recruiter/src/app/app-routing.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-<<<<<<< HEAD
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-
-const routes: Routes = [
-  {path: 'nav-bar', component: NavbarComponent},
-  {path: 'admin-dashboard', component: AdminDashboardComponent},
-=======
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'nav-bar', component: NavbarComponent},
-  {path: 'login', component: LoginComponent}
->>>>>>> 0bcacdc18f5ad543816012586820a35ebc45d03c
+  {path: 'admin-dashboard', component: AdminDashboardComponent},
+  {path: 'login', component: LoginComponent},
+
 ];
 
 @NgModule({
