76c3e71cd61f910fc08240d10225f7a50a96fec0
SethmaNW <your-email@example.com>
2024-07-25 12:08:19 +0530

    Login page updated

--- smart-recruiter/src/app/app-routing.module.ts
+++ smart-recruiter/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'nav-bar', component: NavbarComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
