93a6a9db05ad0a96e53adc0f297110f21d6f567c
Ashan-Wimalasiri <e19452@eng.pdn.ac.lk>
2024-07-26 16:32:54 +0530

    new project initialize

--- smart-recruiter/src/app/app.component.spec.ts
+++ smart-recruiter/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'smart_recruiter'`, () => {
+  it(`should have as title 'smart-recruiter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('smart_recruiter');
+    expect(app.title).toEqual('smart-recruiter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('smart_recruiter app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('smart-recruiter app is running!');
   });
 });
