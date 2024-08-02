3e72fabb18af5a67f55d666346e5d4f59c6db3af
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2024-08-02 10:51:49 +0530

    form styling and search function in admin dashboard

--- FrontEnd/smart-recruiter/src/app/models/applicants.model.ts
+++ FrontEnd/smart-recruiter/src/app/models/applicants.model.ts
@@ -1,10 +1,11 @@
-export interface IApplicant {
+export interface Applicant {
     id: number;
     position: string;
     name: string;
     degree: string; 
     contact: string;
     skills: string;
+    startingDate: string;    // is this a date type
     workExperience: string;
     cv: string;
     status: string;
