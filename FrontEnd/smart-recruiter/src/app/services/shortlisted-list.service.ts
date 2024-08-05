aa91c0418bec3b2d78c813a7ed35ce66fa591384
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2024-08-05 21:02:02 +0530

    6 - shortlisted cards

--- FrontEnd/smart-recruiter/src/app/services/shortlisted-list.service.ts
+++ FrontEnd/smart-recruiter/src/app/services/shortlisted-list.service.ts
@@ -4,6 +4,66 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ShortlistedListService {
+  shortlistedCandidates: any[] = [];
+  loading: boolean = true;
 
-  constructor() { }
+  constructor() { 
+    this.loadShortlisted();
+  }
+
+  getShortlistedCandidates() {
+    return this.shortlistedCandidates
+  }
+
+  isLoading() {
+    return this.loading;
+  }
+
+  loadShortlisted(){
+    this.shortlistedCandidates = [
+      {
+        id: 1,
+        position: 'Software Engineer',
+        name: 'John Doe',
+        availableDate: '2021-01-01',
+        university: 'University of Colombo',
+        degree: 'BSc in Computer Science',
+        contact: '077 0089633',
+        email: 'joe.email.com',
+        skills: 'java, angular, spring, khfsdgwe yoisdjgk kjgfsdgwe yoisdj gkkjg',
+        gpa: '3.5',
+        workExperience: '2 years',
+        cv: 'https://www.google.com', 
+        comment: '',   
+        testmark: '20',
+        interview1: 'the action of ordaining someone in holy orders',
+        interview2: 'the action of ordaining someone in holy orders',	
+        interview3: 'the action of ordaining someone in holy orders',       
+        commentExceeded: false,
+        status: 'Shortlisted'
+      },
+      {
+        id: 2,
+        position: 'Software Engineer',
+        name: 'Keran Perera',
+        availableDate: '2021-01-01',
+        university: 'University of Colombo',
+        degree: 'BSc in Physics',
+        contact: '077 0089633',
+        email: 'karan.email.com',
+        skills: 'java, angular, spring, khfs dgweyoisd jgkkjgf sdgwey oisdjgkk jgfsdgwe yoisdj gkkjg yoisdjgk kjgfsdgwe yoisdj gkkjg..',
+        gpa: '3.5',
+        workExperience: '2 years',
+        cv: 'https://www.google.com',   
+        comment: 'comment 1',  
+        interview1: '',
+        interview2: '',	
+        interview3: '',        
+        commentExceeded: false,
+        status: 'Shortlisted'
+      }
+    ];
+
+    this.loading = false;
+  }
 }
--- FrontEnd/smart-recruiter/src/app/shortlisted-candidate-account/shortlisted-candidate-account.component.ts
+++ FrontEnd/smart-recruiter/src/app/shortlisted-candidate-account/shortlisted-candidate-account.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShortlistedListService } from '../services/shortlisted-list.service';
 
 @Component({
   selector: 'app-shortlisted-candidate-account',
   templateUrl: './shortlisted-candidate-account.component.html',
   styleUrls: ['./shortlisted-candidate-account.component.scss']
 })
-export class ShortlistedCandidateAccountComponent {
+export class ShortlistedCandidateAccountComponent implements OnInit {
+  shortlisted: any[] = [];
+  loading: boolean = true;
 
+  constructor(private shortlistedListService: ShortlistedListService) { }
+
+  ngOnInit(){
+    this.shortlisted = this.shortlistedListService.getShortlistedCandidates();
+    this.loading = this.shortlistedListService.isLoading();
+  }
 }
