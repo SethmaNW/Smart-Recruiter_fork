3289bf5db16696655854c3feb64638ef21231f01
Ashan-Wimalasiri <e19452@eng.pdn.ac.lk>
2024-08-06 17:10:28 +0530

    backed initiated

---
+++ BackEnd/Services/CandidateService.cs
@@ -0,0 +1,23 @@
+﻿namespace Services;
+
+using Repositories;
+
+public interface ICandidateService
+{
+    string GetAll();
+}
+public class CandidateService : ICandidateService
+{
+    private readonly ICandidateRepository _CandidateRepository;
+
+    public CandidateService(
+        ICandidateRepository candidateRepository
+    ){
+        _CandidateRepository = candidateRepository;
+    }
+    public string GetAll()
+    {
+        return _CandidateRepository.GetAll();
+    }
+}
+
