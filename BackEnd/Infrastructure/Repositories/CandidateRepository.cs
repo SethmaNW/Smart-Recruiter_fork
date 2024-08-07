6cc5a1210cf79530b73ca04c1fd36fbdb413bf8d
Ashan Wimalasiri <ashan.wimalasiri2@gmail.com>
2024-08-07 22:04:13 +0530

    gitignore file added

--- BackEnd/Infrastructure/Repositories/CandidateRepository.cs
+++ BackEnd/Infrastructure/Repositories/CandidateRepository.cs
@@ -1,10 +1,22 @@
+using Dapper;
 using Domain.RepositoryInterfaces;
+using Infrastructure.DBConnection;
 
 namespace Infrastructure.Repositories;
 public class CandidateRepository : ICandidateRepository
 {
+    private DBContext _dbContext;
+    public CandidateRepository(DBContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
     public string GetAll()
     {
-        return "Hello from CandidatesRepository!";
+        using var connection = _dbContext.GetOpenConnection();
+        var sql = """
+            SELECT Description FROM dbo.roles WHERE Id = 1
+        """;
+        return connection.QuerySingleOrDefault<string>(sql);
+        //return "Hello from CandidatesRepository!";
     }
 }
