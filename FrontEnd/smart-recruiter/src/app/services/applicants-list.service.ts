52a68d8504de1132358144e017ef04154a0f2e1c
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2024-08-09 09:09:20 +0530

    applicant page backend

--- FrontEnd/smart-recruiter/src/app/services/applicants-list.service.ts
+++ FrontEnd/smart-recruiter/src/app/services/applicants-list.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Applicant } from '../models/applicants.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +10,17 @@ export class ApplicantsListService {
   customers: any[] = [];
   loading: boolean = true;
 
-  constructor() {
-    this.loadDummyData();
+  constructor(private http: HttpClient) {
+    // this.loadDummyData();
   }
 
-  getCustomers() {
-    return this.customers;
-  }
+  // getCustomers() {
+  //   return this.customers;
+  // }
 
-  isLoading() {
-    return this.loading;
-  }
+  // isLoading() {
+  //   return this.loading;
+  // }
 
   updateCommentExceeded(id: number, exceeded: boolean) {
     const customer = this.customers.find(c => c.id === id);
@@ -26,76 +29,80 @@ export class ApplicantsListService {
     }
   }
 
-  loadDummyData() {
-    this.customers = [
-      {
-        // representative: { name: 'Jane Smith' },
-        id: 1,
-        position: 'Software Engineer',
-        name: 'John Doe',
-        availableDate: '2021-01-01',
-        university: 'University of Colombo',
-        degree: 'BSc in Computer Science',
-        contact: '077 0089633',
-        skills: 'java, angular, spring, khfsdgwe yoisdjgk kjgfsdgwe yoisdj gkkjg',
-        gpa: '3.5',
-        workExperience: '2 years',
-        cv: 'https://www.google.com', 
-        comment: '',          
-        commentExceeded: false,
-        status: 'Applied'
-      },
-      {
-        id: 2,
-        position: 'Software Engineer',
-        name: 'Keran Perera',
-        availableDate: '2021-01-01',
-        university: 'University of Colombo',
-        degree: 'BSc in Physics',
-        contact: '077 0089633',
-        skills: 'java, angular, spring, khfs dgweyoisd jgkkjgf sdgwey oisdjgkk jgfsdgwe yoisdj gkkjg yoisdjgk kjgfsdgwe yoisdj gkkjg..',
-        gpa: '3.5',
-        workExperience: '2 years',
-        cv: 'https://www.google.com',   
-        comment: 'comment 1',   
-        commentExceeded: false,     
-        status: 'ShortListed'
-      },
-      {
-        id: 3,
-        position: 'Software Engineer',
-        name: 'Keran Perera',
-        availableDate: '2021-01-01',
-        university: 'University of Colombo',
-        degree: 'BSc in Physics',
-        contact: '077 0089633',
-        skills: 'java, angular, spring, khfsdgweyoisdjgkkjg',
-        gpa: '3.5',
-        workExperience: '2 years',
-        cv: 'https://www.google.com',   
-        comment: '',     
-        commentExceeded: false,
-        status: 'Not Selected'
-      },
-      {
-        id: 4,
-        position: 'Software Engineer',
-        name: 'Keran Perera',
-        availableDate: '2021-01-01',
-        university: 'University of Colombo',
-        degree: 'BSc in Physics',
-        contact: '077 0089633',
-        skills: 'java, angular, spring, khfsdgweyoisdjgkkjg',
-        gpa: '3.5',
-        workExperience: '2 years',
-        cv: 'https://www.google.com',        
-        comment: '',   
-        commentExceeded: false,
-        status: 'Applied'
-      }
-    ];
+  // loadDummyData() {
+  //   this.customers = [
+  //     {
+  //       // representative: { name: 'Jane Smith' },
+  //       id: 1,
+  //       position: 'Software Engineer',
+  //       name: 'John Doe',
+  //       availableDate: '2021-01-01',
+  //       university: 'University of Colombo',
+  //       degree: 'BSc in Computer Science',
+  //       contact: '077 0089633',
+  //       skills: 'java, angular, spring, khfsdgwe yoisdjgk kjgfsdgwe yoisdj gkkjg',
+  //       gpa: '3.5',
+  //       workExperience: '2 years',
+  //       cv: 'https://www.google.com', 
+  //       comment: '',          
+  //       commentExceeded: false,
+  //       status: 'Applied'
+  //     },
+  //     {
+  //       id: 2,
+  //       position: 'Software Engineer',
+  //       name: 'Keran Perera',
+  //       availableDate: '2021-01-01',
+  //       university: 'University of Colombo',
+  //       degree: 'BSc in Physics',
+  //       contact: '077 0089633',
+  //       skills: 'java, angular, spring, khfs dgweyoisd jgkkjgf sdgwey oisdjgkk jgfsdgwe yoisdj gkkjg yoisdjgk kjgfsdgwe yoisdj gkkjg..',
+  //       gpa: '3.5',
+  //       workExperience: '2 years',
+  //       cv: 'https://www.google.com',   
+  //       comment: 'comment 1',   
+  //       commentExceeded: false,     
+  //       status: 'ShortListed'
+  //     },
+  //     {
+  //       id: 3,
+  //       position: 'Software Engineer',
+  //       name: 'Keran Perera',
+  //       availableDate: '2021-01-01',
+  //       university: 'University of Colombo',
+  //       degree: 'BSc in Physics',
+  //       contact: '077 0089633',
+  //       skills: 'java, angular, spring, khfsdgweyoisdjgkkjg',
+  //       gpa: '3.5',
+  //       workExperience: '2 years',
+  //       cv: 'https://www.google.com',   
+  //       comment: '',     
+  //       commentExceeded: false,
+  //       status: 'Not Selected'
+  //     },
+  //     {
+  //       id: 4,
+  //       position: 'Software Engineer',
+  //       name: 'Keran Perera',
+  //       availableDate: '2021-01-01',
+  //       university: 'University of Colombo',
+  //       degree: 'BSc in Physics',
+  //       contact: '077 0089633',
+  //       skills: 'java, angular, spring, khfsdgweyoisdjgkkjg',
+  //       gpa: '3.5',
+  //       workExperience: '2 years',
+  //       cv: 'https://www.google.com',        
+  //       comment: '',   
+  //       commentExceeded: false,
+  //       status: 'Applied'
+  //     }
+  //   ];
 
-    this.loading = false;
+  //   this.loading = false;
+  // }
+
+  getAllApplicants(): Observable<Applicant[]> {
+    return this.http.get<Applicant[]>('api/Candidate');
   }
 
 }
