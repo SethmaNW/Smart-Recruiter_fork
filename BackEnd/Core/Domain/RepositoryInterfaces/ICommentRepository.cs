f6a5553e3fdde49832abb5b4fc10c2f1b571a107
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2024-08-22 12:16:06 +0530

    complete comment column in applicant table

--- BackEnd/Core/Domain/RepositoryInterfaces/ICommentRepository.cs
+++ BackEnd/Core/Domain/RepositoryInterfaces/ICommentRepository.cs
@@ -6,4 +6,5 @@ public interface ICommentRepository
 {
     Task<bool> UpdateApplicantComment(int jobId, int candidateId, int adminId, int roleId, string commentText);
     Task<bool> CheckApplicantComment(int jobId, int candidateId);
+    Task<List<int>> GetCandidateIdsOfJobId(int jobId); 
 }
