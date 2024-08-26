016f0e577b4b7834ff23dce1d120da94fe85d4fb
Ashan Wimalasiri <ashanwim@lbfinance.local>
2024-08-26 15:58:59 +0530

    pie chart added

---
+++ BackEnd/Core/DTO/DTOs/MarkOutputDTO.cs
@@ -0,0 +1,17 @@
+namespace DTO.DTOs;
+
+public class MarkOutputDTO
+{
+    public decimal FinalScore { get; set; }
+    public decimal TotalMark { get; set; }
+    public decimal AttitudeAndDisciplineParticipation { get; set; }
+    public decimal TechnicalKnowledgeParticipation { get; set; }
+    public decimal EducationBackgroundParticipation { get; set; }
+    public decimal ProfessionalQualificationParticipation { get; set; }
+    public decimal CareerBackgroundParticipation { get; set; }
+    public decimal CommunicationSkillsParticipation { get; set; }
+    public decimal CulturalFitParticipation { get; set; }
+    public decimal FamilyBackgroundParticipation { get; set; }
+    public decimal IQCreativityProblemSolvingSkillsParticipation { get; set; }
+    public decimal ManagementSkillsParticipation { get; set; }
+}
