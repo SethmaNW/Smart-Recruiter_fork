06498528ba97ef2b38b5ed36b55535fe9f4c9beb
Ashan Wimalasiri <ashanwim@lbfinance.local>
2024-08-26 22:29:29 +0530

    doughnut and knob completed

--- FrontEnd/smart-recruiter/src/app/pie-chart/pie-chart.component.ts
+++ FrontEnd/smart-recruiter/src/app/pie-chart/pie-chart.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, AfterViewInit,ViewChild,ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit,ViewChild,ElementRef, ViewEncapsulation } from '@angular/core';
 import { SlidersService } from '../services/sliders.service';
-import { Chart } from 'chart.js';
+import { Chart, Legend } from 'chart.js';
 import { PieChartService } from './pie-chart.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -8,12 +8,14 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.scss']
+  styleUrls: ['./pie-chart.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class PieChartComponent implements OnInit {
   // for tab view
-  public tabs: { title: string, content: string, isDisabled : boolean, data : any }[] = [];
+  public tabs: { title: string, isDisabled : boolean, data : any, options : any, finalScore : number | undefined }[] = [];
+  public options: any;
   //For query parameters
   public jobId : number | undefined;
   public candidateId : number | undefined;
@@ -25,6 +27,7 @@ export class PieChartComponent implements OnInit {
   }
 
   public initializer() : void {
+    
     // Fetch the query parameter
     this.route.queryParams.subscribe(params => {
       this.jobId = params['jobId'];
@@ -32,14 +35,12 @@ export class PieChartComponent implements OnInit {
     });
 
     this.tabs = [
-      { title: '1st', content: 'Tab 1 Content', isDisabled : false, data : {} },
-      { title: '2nd', content: 'Tab 2 Content', isDisabled : false, data : {} },
-      { title: '3rd', content: 'Tab 3 Content', isDisabled : false, data : {} }
+      { title: '1st', isDisabled : false, data : {}, options : {}, finalScore : undefined },
+      { title: '2nd', isDisabled : false, data : {}, options : {}, finalScore : undefined },
+      { title: '3rd', isDisabled : false, data : {}, options : {}, finalScore : undefined },
     ];
 
     // fetch the marks of the candidate for all roles (1st interview, 2nd interview, etc)
-
-    const roleIds : number[] = [1, 2, 3];
 
     this.tabs.forEach((tab, index) => {
       this.PieChartSVC.getMarks(this.candidateId, this.jobId, index+1).subscribe((res) => {
@@ -48,40 +49,78 @@ export class PieChartComponent implements OnInit {
         }
         else{
           //create the data for pie chart
+          const labels = ['Attitude and Discipline', 'Technical Knowledge', 'Education Background', 'Professional Qualification', 'Career Background', 'Communication Skills', 'Cultural Fit', 'Family Background', 'IQ/Creativity/Problem Solving Skills', 'Management Skills'];
+          //const dataPoints = [res.attitudeAndDisciplineParticipation, res.technicalKnowledgeParticipation, res.educationBackgroundParticipation, res.professionalQualificationParticipation, res.careerBackgroundParticipation, res.communicationSkillsParticipation, res.culturalFitParticipation, res.familyBackgroundParticipation, res.iqCreativityProblemSolvingSkillsParticipation, res.managementSkillsParticipation];
+          const dataPoints = [
+            res.attitudeAndDisciplineParticipation, 
+            res.technicalKnowledgeParticipation, 
+            res.educationBackgroundParticipation, 
+            res.professionalQualificationParticipation, 
+            res.careerBackgroundParticipation, 
+            res.communicationSkillsParticipation, 
+            res.culturalFitParticipation, 
+            res.familyBackgroundParticipation, 
+            res.iqCreativityProblemSolvingSkillsParticipation, 
+            res.managementSkillsParticipation
+          ].map((point: number | undefined) => point ? point * 100 : 0); // Multiply each data point by 100 if it is not undefined
+          const backgroundColors = ['#FF6384', '#36A2EB', '#FFCE56', '#E7E9ED', '#4CAF50','#FF8A80','#FFD180','#A1887F','#90A4AE','#FF6652'];
+          const hoverBackgroundColors = ['#FF6384', '#36A2EB', '#FFCE56', '#E7E9ED', '#4CAF50','#FF8A80','#FFD180','#A1887F','#90A4AE','#FF6652'];
+
+          const filteredLabels = [];
+          const filteredData = [];
+          const filteredBackgroundColors = [];
+          const filteredHoverBackgroundColors = [];
+
+          for (let i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] !== 0) {
+              filteredLabels.push(labels[i]);
+              filteredData.push(dataPoints[i]);
+              filteredBackgroundColors.push(backgroundColors[i]);
+              filteredHoverBackgroundColors.push(hoverBackgroundColors[i]);
+            }
+          }
+
           const data = {
-            labels: ['Attitude and Discipline', 'Technical Knowledge', 'Education Background', 'Professional Qualification', 'Career Background', 'Communication Skills', 'Cultural Fit', 'Family Background', 'IQ/Creativity/Problem Solving Skills', 'Management Skills'],
+            labels: filteredLabels,
             datasets: [
               {
-                data: [res.attitudeAndDisciplineParticipation, res.technicalKnowledgeParticipation, res.educationBackgroundParticipation, res.professionalQualificationParticipation, res.careerBackgroundParticipation, res.communicationSkillsParticipation, res.culturalFitParticipation, res.familyBackgroundParticipation, res.iqCreativityProblemSolvingSkillsParticipation, res.managementSkillsParticipation],
-                backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#E7E9ED', '#4CAF50','#FF8A80','#FFD180','#A1887F','#90A4AE','#FF6652'],
-                hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#E7E9ED', '#4CAF50','#FF8A80','#FFD180','#A1887F','#90A4AE','#FF6652']
+                data: filteredData,
+                backgroundColor: filteredBackgroundColors,
+                hoverBackgroundColor: filteredHoverBackgroundColors
               }
             ]
           };
+
+          this.options = {
+            //display labels on data elements in graph
+            plugins: {
+              datalabels: {
+                align: 'end',
+                anchor: 'end',
+                borderRadius: 4,
+                backgroundColor: 'teal',
+                color: 'white',
+                font: {
+                  weight: 'bold',
+                },
+              },
+              
+              legend: {
+                display: true,
+                position: 'bottom',
+                align : 'start',
+              },
+            },
+          };
+          this.tabs[index].finalScore = Math.round((res.finalScore? res.finalScore : 0) * 10) / 10;
+          this.tabs[index].options = this.options;
           this.tabs[index].data = data;
         }
       });
     });
 
-    // const roleId1 = 1;
-    // const roleId2 = 2;
-    // const roleId3 = 3;
-
-    // this.PieChartSVC.getMarks(this.candidateId, this.jobId, roleId1).subscribe((res) => {
-    //   //console.log(res);
-    // });
-
-    // this.PieChartSVC.getMarks(this.candidateId, this.jobId, roleId2).subscribe((res) => {
-    //   console.log(res);
-    // });
-
-    // this.PieChartSVC.getMarks(this.candidateId, this.jobId, roleId3).subscribe((res) => {
-    //   //console.log(res);
-    // });
+    
   }
-
-  
-
 }
 
 // export class PieChartComponent implements OnInit, AfterViewInit {
