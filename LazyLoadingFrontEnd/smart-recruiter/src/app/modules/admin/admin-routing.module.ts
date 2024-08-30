cd66028993056215f7b1da5f1a61e30e45f32db2
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2024-08-30 15:04:56 +0530

    import modules related to applicants page

--- LazyLoadingFrontEnd/smart-recruiter/src/app/modules/admin/admin-routing.module.ts
+++ LazyLoadingFrontEnd/smart-recruiter/src/app/modules/admin/admin-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { ApplicantsComponent } from './components/applicants/applicants.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'applicants',
+    pathMatch: 'full'
+  },
   {
     path: 'admin-dashboard',
     component: AdminDashboardComponent
   },
+  {
+    path: 'applicants',
+    component: ApplicantsComponent
+  }
 ];
 
 @NgModule({
