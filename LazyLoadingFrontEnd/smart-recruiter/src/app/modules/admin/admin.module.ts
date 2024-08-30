ac64a9f7f227360bff3b18cbd937b812b774f745
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2024-08-30 16:48:52 +0530

    complete imports in applicants page

--- LazyLoadingFrontEnd/smart-recruiter/src/app/modules/admin/admin.module.ts
+++ LazyLoadingFrontEnd/smart-recruiter/src/app/modules/admin/admin.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ConfirmationService, SharedModule } from 'primeng/api';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
+import { DropdownModule } from 'primeng/dropdown';
 import { AdminRoutingModule } from './admin-routing.module';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
+import { ToastModule } from 'primeng/toast';
+import { TabViewModule } from 'primeng/tabview';
+
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { JobPostComponent } from './components/job-post/job-post.component';
 import { ShortlistedCandidateAccountComponent } from './components/shortlisted-candidate-account/shortlisted-candidate-account.component';
@@ -10,14 +21,7 @@ import { ApplicantsComponent } from './components/applicants/applicants.componen
 import { ApplicantsTableComponent } from './components/applicants-table/applicants-table.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { SharedModule } from 'primeng/api';
-import { CardModule } from 'primeng/card';
-import { TagModule } from 'primeng/tag';
-import { DropdownModule } from 'primeng/dropdown';
-import { RouterLink } from '@angular/router';
-import { DialogModule } from 'primeng/dialog';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { TableModule } from 'primeng/table';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 
 @NgModule({
@@ -33,17 +37,20 @@ import { TableModule } from 'primeng/table';
   ],
   imports: [
     CommonModule,
-    SharedModule,
     FormsModule,
     AdminRoutingModule,
     SharedModule,
     CardModule,
     TagModule,
     DropdownModule,
-    RouterLink,
-    DialogModule,
+    TableModule,
+    ButtonModule,
     ConfirmDialogModule,
-    TableModule
-  ]
+    DialogModule,
+    ToastModule,
+    MultiSelectModule,
+    TabViewModule
+  ],
+  providers: [ConfirmationService],
 })
 export class AdminModule { }
