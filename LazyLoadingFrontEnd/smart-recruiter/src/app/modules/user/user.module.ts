a44e5c097d4ccde825b7bf12315354aa67fb6c61
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2024-09-03 08:57:54 +0530

    fix user routing

--- LazyLoadingFrontEnd/smart-recruiter/src/app/modules/user/user.module.ts
+++ LazyLoadingFrontEnd/smart-recruiter/src/app/modules/user/user.module.ts
@@ -14,7 +14,14 @@ import { FileUploadModule } from 'primeng/fileupload';
 import { CalendarModule } from 'primeng/calendar';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: 'form',
+    component: FormComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -34,7 +41,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     InputTextareaModule,
     CalendarModule,
     ReactiveFormsModule,
-    SharedModule
+    SharedModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class UserModule { }
